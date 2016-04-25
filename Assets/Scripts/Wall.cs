636c42ce93a0a48f7da3bbc6a7805b04a2b4d205
Eanna MacDonough <eannaaacdonoughgti@gmail.com>
2016-04-25 16:35:55 +0100

    Project Upload

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    public int hp = 4;
+    public AudioClip cutSound1;
+    public AudioClip cutSound2;
+
+    private SpriteRenderer spriteRenderer;
+
+	void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();	
+	}
+	
+    public void DamageWall(int loss)
+    {
+        SoundManager.instance.RandomiseSfx(cutSound1, cutSound2);
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
+}
