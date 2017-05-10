4607014706fe7dfce05a4b0c1edc5f7eb818a515
EMDGMIT <eannamacdonoughgti@gmail.com>
2017-05-10 11:13:53 +0100

    Add files via upload

---
+++ Assets/Scripts/TouchControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControls : MonoBehaviour {
+
+    private PauseMenu thePauseMenu;
+
+
+	void Start()
+    {
+        thePauseMenu = FindObjectOfType<PauseMenu>();
+    }
+	
+	
+	public void Pause()
+    {
+        thePauseMenu.PauseUnpause();
+	}
+}
